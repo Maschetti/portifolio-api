9841217da89a8cdd096b3c42b6cb21d4a668b167
Maschetti <mateusvmaschietto@gmail.com>
2025-03-09 20:21:27 -0300

    chore: testing the deploys

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ app.get('/', async (request, reply) => {
   return "API is running!"
 })
 
+app.get('/teste', async (request, reply) => {
+  return "Teste"
+})
+
 app.listen({port: 3333}).then(() => {
   console.log(`Server is running on port: 3333`)
 })
