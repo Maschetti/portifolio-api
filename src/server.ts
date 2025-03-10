d2e4c786099f87e61b0568cbaf5ab38b68ddca7a
Maschetti <mateusvmaschietto@gmail.com>
2025-03-10 09:44:05 -0300

    Revert "chore: testing the deploys"
    This reverts commit 9841217da89a8cdd096b3c42b6cb21d4a668b167.

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,6 @@ app.get('/', async (request, reply) => {
   return "API is running!"
 })
 
-app.get('/teste', async (request, reply) => {
-  return "Teste"
-})
-
 app.listen({port: 3333}).then(() => {
   console.log(`Server is running on port: 3333`)
 })
